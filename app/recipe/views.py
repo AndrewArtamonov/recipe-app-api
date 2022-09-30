c6fbccea023e17d891395449740d0cf2140294fe
Andriy Artamonov <andrew.artamonov@thoughtfulautomation.com>
2022-09-30 17:39:21 +0300

    feat: add ingredients endpoints & tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -43,5 +43,20 @@ class TagViewSet(
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve tags for authenticated user."""
+        """Filter tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(
+    mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    """Manage ingredients in the database"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter ingredients for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
